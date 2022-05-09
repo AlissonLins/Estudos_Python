24b17a55774eb26b36acf9b773d8d5cb3353f81a
Alisson Lins <91570611+AlissonLins@users.noreply.github.com>
2022-05-09 18:01:00 -0300

    Update Atividade34.py

--- Atividade34.py
+++ Atividade34.py
@@ -1,6 +1,8 @@
 salário = float(input('Informe o salário do funcionário desejado: R$'))
 if salário <= 1250:
     novo = salário + (salário * 15 / 100)
+elif salário == 1500:
+    novo = salário + (salário * 7 / 100)
 else:
     novo = salário + (salário * 10 / 100)
 print('Este funcionário ganhava R${:.2f} e passará a ganhar R${:.2f} agora.'.format(salário, novo))
